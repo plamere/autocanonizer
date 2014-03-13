d97e44076c3747a99a7324ab05bfd2dc50ffe173
Paul Lamere <Paul.Lamere@gmail.com>
2014-03-13 10:04:36 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# The Autocanonizer
+
+[The Autocanonizer](http://static.echonest.com/autocanonizer/index.html "") is a web app that
+turns any song into a canon.
+
+It uses The Echo Nest API to analyze a track and break them down into bars, beats, tatums, and 
+then uses this data to create an auto-canon of the song.
---
+++ js/favsongs.js
@@ -0,0 +1,38 @@
+var favSongs = [
+    {
+        "artist": "Weezer", 
+        "id": "TRQWWGY13B03FB1D48", 
+        "title": "El Scorcho", 
+        "visits": 62
+    }, 
+    {
+        "artist": "Skrillex", 
+        "id": "TRQEJOQ13B0F8E07E5", 
+        "title": "Levels (Skrillex Remix)", 
+        "visits": 110
+    }, 
+    {
+        "artist": "The Beatles", 
+        "id": "TRJQDYU14404EDA900", 
+        "title": "Hey Jude", 
+        "visits": 62
+    }, 
+    {
+        "artist": "The White Stripes", 
+        "id": "TRMQYSY13B4C10127E", 
+        "title": "Seven Nation Army", 
+        "visits": 29
+    }, 
+    {
+        "artist": "Daft Punk", 
+        "id": "TRSRSLI13451D2B044", 
+        "title": "Around The World", 
+        "visits": 29
+    }, 
+    {
+        "artist": "Skrillex", 
+        "id": "TRPNLNS13991BF1FBC", 
+        "title": "Bangarang (feat. Sirah)", 
+        "visits": 26
+    }, 
+];

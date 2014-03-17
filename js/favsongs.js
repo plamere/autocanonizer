d325cada18f781ad3d9d57a702b6e1e90a81b4d7
Paul Lamere <Paul.Lamere@gmail.com>
2014-03-17 11:13:34 -0400

    Improved visualizer with loud graph and animations

--- js/favsongs.js
+++ js/favsongs.js
@@ -1,15 +1,15 @@
 var favSongs = [
     {
-        "artist": "Weezer", 
-        "id": "TRQWWGY13B03FB1D48", 
-        "title": "El Scorcho", 
-        "visits": 62
-    }, 
-    {
         "artist": "Adele", 
         "id": "TRLZWAN142E39CDD1A", 
         "title": "Someone Like You", 
         "visits": 110
+    }, 
+    {
+        "artist": "Israel Kamakawiwo'ole", 
+        "id": "TRQPKGR1428F4F24A0", 
+        "title": "Over The Rainbow", 
+        "visits": 62
     }, 
     {
         "artist": "Guns N' Roses", 
@@ -18,16 +18,16 @@ var favSongs = [
         "visits": 110
     }, 
     {
+        "artist": "Weezer", 
+        "id": "TRQWWGY13B03FB1D48", 
+        "title": "El Scorcho", 
+        "visits": 62
+    }, 
+    {
         "artist": "Skrillex", 
         "id": "TRQEJOQ13B0F8E07E5", 
         "title": "Levels (Skrillex Remix)", 
         "visits": 110
-    }, 
-    {
-        "artist": "Israel Kamakawiwo'ole", 
-        "id": "TRQPKGR1428F4F24A0", 
-        "title": "Over The Rainbow", 
-        "visits": 62
     }, 
     {
         "artist": "The Beatles", 
@@ -42,6 +42,24 @@ var favSongs = [
         "visits": 62
     }, 
     {
+        "artist": "One Direction", 
+        "id": "TRKRHII14406C209F2", 
+        "title": "One Thing (Acoustic)", 
+        "visits": 29
+    }, 
+    {
+        "artist": "Karmin", 
+        "id": "TRKBGCP144052FE138", 
+        "title": "Acapella", 
+        "visits": 29
+    }, 
+    {
+        "artist": "Anna Kendrick", 
+        "id": "TRWNGAY13AFC22E72B", 
+        "title": "Cups", 
+        "visits": 29
+    }, 
+    {
         "artist": "The White Stripes", 
         "id": "TRMQYSY13B4C10127E", 
         "title": "Seven Nation Army", 
@@ -51,6 +69,12 @@ var favSongs = [
         "artist": "Robin Thicke", 
         "id": "TRERLFI13F014DF2AD", 
         "title": "Blurred Lines", 
+        "visits": 29
+    }, 
+    {
+        "artist": "Anna Kendrick", 
+        "id": "TRZFDZY13FC58014DA", 
+        "title": "Cups (Pop Version)", 
         "visits": 29
     }, 
     {
